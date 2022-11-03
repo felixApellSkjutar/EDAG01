306bfa889be25d7e8d703d6dae5621aae383af20
Axel Nåsell <axel.nasell@gmail.com>
2022-11-03 13:18:32 +0100

    added matrix making method

--- Lab1/intopt.c
+++ Lab1/intopt.c
@@ -9,3 +9,13 @@ int main()
 	printf("m: %d\nn: %d\n", m, n);
 	return 0;
 }
+
+double** make_matrix(int m, int n)
+{
+	double** a;
+	int i;
+	a = calloc(m, sizeof(double*));
+	for (i = 0; i < m; i += 1)
+		a[i] = calloc(n, sizeof(double));
+	return a;
+}
