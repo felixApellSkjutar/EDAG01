f934beeab01ca9dc8343060f0324cf356dfc4133
Axel Nåsell <axel.nasell@gmail.com>
2022-11-15 12:56:57 +0100

    kod

--- Lab2/simplex.c
+++ Lab2/simplex.c
@@ -1,6 +1,10 @@
-
-
-
-int main() {
-    return 0;
+struct simplex_t {
+int m ; /* Constraints. */
+int n ; /* Decision variables. */
+int var [n+m+1]; /* 0..n  1 are nonbasic. */
+double a [m][n+1]; /* A. */
+double b [m]; /* b. */
+double x [n+1]; /* x. */
+double c [n]; /* c. */
+double y ; /* y. */
 }
