0e9060bb8582f989a343209402476e0a8fb680da
FelixApellSkjutar <felix.apellskjutar@gmail.com>
2022-11-15 14:26:32 +0100

    pivot

--- Lab2/simplex.c
+++ Lab2/simplex.c
@@ -1,37 +1,98 @@
 #include <stdlib.h>
 #include <math.h>
 
-
 typedef struct simplex_t simplex_t;
 
-struct simplex_t {
-    int m ; /* Constraints. */
-    int n ; /* Decision variables. */
-    int *var; /* 0..n  1 are nonbasic. */
-    double *a ; /* A. */
-    double *b ; /* b. */
-    double *x ; /* x. */
-    double *c ; /* c. */
-    double *y ; /* y. */
-}; 
-
-
-int xsimplex(int m, int n, double* a, double* b, double c, double* x, double y, int* var, int h)
+struct simplex_t
 {
-    simplex_t* s;
+    int m;      /* Constraints. */
+    int n;      /* Decision variables. */
+    int *var;   /* 0..n  1 are nonbasic. */
+    double **a; /* A. */
+    double *b;  /* b. */
+    double *x;  /* x. */
+    double *c;  /* c. */
+    double y;   /* y. */
+};
+
+void pivot(simplex_t *s, int row, int col)
+{
+    double **a = s->a;
+    double *b = s->b;
+    double *c = s->c;
+
+    int m = s->m;
+    int n = s->n;
+    int i, j, t;
+
+    t = s->var[col];
+    s->var[col] = s->var[n + row];
+    s->var[n + row] = t;
+    s->y = s->y + c[col] * b[row] / a[row][col];
+
+    for (i = 0; i < n; i += 1)
+    {
+        if (i != col)
+        {
+            c[i] = c[i] - c[col] * a[row][i] / a[row][col];
+        }
+    }
+    c[col] = -c[col] / a[row][col];
+    for (i = 0; i < m; i += 1)
+    {
+        if (i != row)
+        {
+            b[i] = b[i] - a[i][col] * b[row] / a[row][col];
+        }
+    }
+    for (i = 0; i < m; i += 1)
+    {
+        if (i != row)
+        {
+            for (j = 0; j < n; j += 1)
+            {
+                if (j != col)
+                {
+                    a[i][j] = a[i][j] - a[i][col] * a[row][j] / a[row][col];
+                }
+            }
+        }
+    }
+    for (i = 0; i < m; i += 1)
+    {
+        if (i != row)
+        {
+            a[i][col] = -a[i][col] / a[row][col];
+        }
+    }
+    for (i = 0; i < n; i += 1)
+    {
+        if (i != col)
+        {
+            a[row][i] = a[row][i] / a[row][col];
+        }
+    }
+    b[row] = b[row] / a[row][col];
+    a[row][col] = 1 / a[row][col];
+}
+
+int xsimplex(int m, int n, double **a, double *b, double c, double *x, double y, int *var, int h)
+{
+    simplex_t *s;
     int i, row, col;
     if (!initial(&s, m, n, a, b, c, x, y, var))
     {
         free(s->var);
         return NAN;
     }
-    while((col = select_nonbasic(&s))>= 0 )
+    while ((col = select_nonbasic(&s)) >= 0)
     {
         row -= 1;
         int i;
-        for(i = 0; i < m; i+=1)
+        for (i = 0; i < m; i += 1)
         {
-            if((&a)[i][col] > pow(10,-6) && (row < 0 || b[i]/(&a)[i][col] < b[row]/(&a)[row][col])) {
+            if (a[i][col] > pow(10, -6) && (row < 0 || b[i] / a[i][col] < b[row] / a[row][col]))
+            {
                 row = i;
             }
         }
@@ -42,37 +103,39 @@ int xsimplex(int m, int n, double* a, double* b, double c, double* x, double y, 
         }
         pivot(&s, row, col);
     }
-    if (h == 0) 
+    if (h == 0)
     {
-        for(i = 0; i < n; i +=1)
+        for (i = 0; i < n; i += 1)
         {
-            if(s -> var[i] < n)
+            if (s->var[i] < n)
             {
                 x[s->var[i]] = 0;
             }
         }
-        for(i = 0; i < n; i +=1)
+        for (i = 0; i < n; i += 1)
         {
-            if(s->var[n+i] < n)
+            if (s->var[n + i] < n)
             {
-                x[s->var[n+i]] = s->b[i];
+                x[s->var[n + i]] = s->b[i];
             }
         }
         free(s->var);
     }
-    else{
-        for(i = 0; i < n; i +=1)
+    else
+    {
+        for (i = 0; i < n; i += 1)
         {
             x[i] = 0;
         }
-        for(i = n; i < n + m; i += 1)
+        for (i = n; i < n + m; i += 1)
         {
-            x[i] = s->b[i-n];
+            x[i] = s->b[i - n];
         }
     }
     return s->y;
 }
 
-int simplex(int m, int n, double* a, double* b, double c, double* x, double y) {
+int simplex(int m, int n, double *a, double *b, double c, double *x, double y)
+{
     return xsimplex(m, n, a, b, c, x, y, NULL, 0);
 }
